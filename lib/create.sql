7bfe6459c648a82cee917d28f578b9d7955c9859
babyshoes <dianecai@gmail.com>
2015-08-15 16:57:42 -0400

    all done

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,18 @@
--- your code here
+CREATE TABLE projects
+(id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal INTEGER,
+start_date DATE,
+end_date DATE);
+
+CREATE TABLE users
+(id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER);
+
+CREATE TABLE pledges
+(id INTEGER PRIMARY KEY,
+amount INTEGER,
+user_id INTEGER REFERENCES users,
+project_id INTEGER REFERENCES projects);
